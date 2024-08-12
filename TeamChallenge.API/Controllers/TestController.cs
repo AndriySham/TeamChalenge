5e3f7904644b335ec816965866bebfda9490b226
Andrey Sham <avshamatrin@gmail.com>
2024-08-12 17:33:17 +0300

    Initial

---
+++ TeamChallenge.API/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TeamChallenge.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var value = new { id = 7 };
+            return Ok(value);
+        }
+    }
+}
